3414bb2d55e633606f563a18d93a82954a0232d3
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2024-06-26 11:42:45 +0200

    Update searchform.php
    corrected the escaping for SVGs using the sanitization function added in functions.php

--- searchform.php
+++ searchform.php
@@ -8,16 +8,19 @@
 
 ?>
 
+<?php
+$search_icon = agapanto_sanitize_svg( agapanto_get_svg( 'search' ) );
+?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label', 'agapanto' ); ?></span>
-		<input type="search" class="search-field"
-			placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'agapanto' ); ?>"
-			value="<?php echo esc_html( get_search_query() ); ?>" name="s"
-			title="<?php echo esc_attr_x( 'Search for:', 'label', 'agapanto' ); ?>" />
-	</label>
-	<button type="submit" class="search-submit">
-		<?php echo wp_kses_post( agapanto_get_svg( 'search' ) ); ?>
-		<span class="screen-reader-text"><?php echo esc_html_x( 'Search', 'submit button', 'agapanto' ); ?></span>
-	</button>
+    <label>
+        <span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label', 'agapanto' ); ?></span>
+        <input type="search" class="search-field"
+            placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'agapanto' ); ?>"
+            value="<?php echo esc_html( get_search_query() ); ?>" name="s"
+            title="<?php echo esc_attr_x( 'Search for:', 'label', 'agapanto' ); ?>" />
+    </label>
+    <button type="submit" class="search-submit">
+        <?php echo $search_icon; ?>
+        <span class="screen-reader-text"><?php echo esc_html_x( 'Search', 'submit button', 'agapanto' ); ?></span>
+    </button>
 </form>
