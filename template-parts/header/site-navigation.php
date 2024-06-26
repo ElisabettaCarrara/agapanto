14c3ee49a599d341218b84f0dcc6880359e7b8da
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2024-06-26 11:58:31 +0200

    Update site-navigation.php
    corrected sanitization for SVG icons

--- template-parts/header/site-navigation.php
+++ template-parts/header/site-navigation.php
@@ -8,12 +8,12 @@
 ?>
 <?php if ( has_nav_menu( 'primary' ) ) : ?>
 	<button class="primary-menu-toggle menu-toggle" aria-controls="primary-menu" aria-expanded="false" <?php agapanto_amp_menu_toggle(); ?>>
-		<?php
-		echo agapanto_sanitize_svg( agapanto_get_svg( 'menu' ) );
-		echo agapanto_sanitize_svg( agapanto_get_svg( 'close' ) );
-		?>
-		<span class="menu-toggle-text screen-reader-text"><?php esc_html_e( 'Menu', 'agapanto' ); ?></span>
-	</button>
+    <?php
+    agapanto_svg_icon('menu');
+    agapanto_svg_icon('close');
+    ?>
+    <span class="menu-toggle-text screen-reader-text"><?php esc_html_e( 'Menu', 'agapanto' ); ?></span>
+</button>
 	<div class="primary-navigation">
 		<nav id="site-navigation" class="main-navigation" role="navigation" <?php agapanto_amp_menu_is_toggled(); ?> aria-label="<?php esc_attr_e( 'Primary Menu', 'agapanto' ); ?>">
 			<?php
