1e1377fa92a0f3fc964e87ed0433e021933e18d8
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2024-07-02 10:54:38 +0200

    Update te be full-with no sidebar
    Made the magazine page full-width with no sidebar

--- template-magazine.php
+++ template-magazine.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<section id="primary" class="fullwidth-content-area content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
@@ -19,7 +19,5 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
-
-	<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
